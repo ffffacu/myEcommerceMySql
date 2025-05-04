7e782bd1e03ba36f8a15c25ab827539760541d7c
ffffacu <ffffacu@hotmail.com>
2025-05-04 18:37:15 -0300

    repart_db7

--- dist/index.js
+++ dist/index.js
@@ -5,13 +5,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const index_routes_1 = __importDefault(require("./routes/index.routes"));
+const db_1 = require("./db");
 const config_1 = __importDefault(require("./config/config"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use(`/api`, index_routes_1.default);
-app.get('/', (req, res) => {
-    res.send('API funcionando sin conexión a DB');
+app.use(`/`, async (req, res) => {
+    try {
+        const [result] = await db_1.pool.query(`SELECT "hello world" as Result`);
+        res.send(result);
+    }
+    catch (error) {
+        console.error(`Error al conectar con la base de datos:${error}`);
+        res.status(500).json({ error: 'No se pudo conectar con la base de datos' });
+    }
 });
 app.listen(config_1.default.PORT, () => {
     console.log(`Servidor corriendo en el puerto ${config_1.default.PORT}`);
