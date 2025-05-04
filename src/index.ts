7f3000d6e1c3d2c036baa246615833c1f0603aef
ffffacu <ffffacu@hotmail.com>
2025-05-04 18:53:36 -0300

    repart_db8

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,33 @@
 import express from 'express';
 import router from './routes/index.routes';
-import {pool} from './db';
+import { pool } from './db';
 import config from './config/config';
+import path from 'path';
+import favicon from 'serve-favicon';
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended:true}));
 
-app.use(`/api`, router)
+app.use(express.static(path.join(__dirname, 'public'))); 
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico'))); 
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(`/api`, router);
+
+
 app.use(`/`, async (req, res) => {
   try {
-    const [result] = await pool.query(`SELECT "hello world" as Result`)
-    res.send(result)
+    const [result] = await pool.query(`SELECT "hello world" as Result`);
+    res.send(result);
   } catch (error) {
     console.error(`Error al conectar con la base de datos:${error}`);
     res.status(500).json({ error: 'No se pudo conectar con la base de datos' });
   }
-})
+});
 
 app.get('/health', (req, res) => {
   res.status(200).send('OK');
