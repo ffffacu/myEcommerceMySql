c78d57ce8f9f3b30f9d465fd34c2c72af72fc3b5
ffffacu <ffffacu@hotmail.com>
2025-05-04 18:25:59 -0300

    repart_db5

--- src/index.ts
+++ src/index.ts
@@ -9,16 +9,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended:true}));
 
 app.use(`/api`, router)
-app.use(`/`, async (req, res) => {
-  try {
-    const [result] = await pool.query(`SELECT "hello world" as Result`)
-    res.send(result)
-  } catch (error) {
-    console.error(`Error al conectar con la base de datos:${error}`);
-    res.status(500).json({ error: 'No se pudo conectar con la base de datos' });
-  }
-})
-
+app.get('/', (req, res) => {
+  res.send('API funcionando sin conexión a DB');
+});
 
 app.listen(config.PORT, () => {
   console.log(`Servidor corriendo en el puerto ${config.PORT}`);
