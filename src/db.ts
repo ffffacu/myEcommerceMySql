8a672e4058ab6f08da9a64a77cccafcbe45c6ed4
ffffacu <ffffacu@hotmail.com>
2025-05-04 19:43:01 -0300

    repart_db13

--- src/db.ts
+++ src/db.ts
@@ -1,13 +1,28 @@
-import { createPool } from "mysql2/promise";
+import { createPool, PoolConnection } from "mysql2/promise";
 import config from "./config/config";
 
-
-
 export const pool = createPool({
-    host: 'switchback.proxy.rlwy.net', // Host de la base de datos
-    user: 'root', // Usuario
-    password: 'rdMpfMjPjzdnlzmBUrRStAEPfjTlzxXA', // Contraseña
-    port: 12592, // Puerto
-    database: 'railway', // Nombre de la base de datos
+    host: config.DB_HOST,
+    user: config.DB_USER,
+    password: config.DB_PASSWORD,
+    port:Number(config.DB_PORT) ,
+    database: config.DB_DATABASE,
 });
 
+async function testConnection() {
+    try {
+        // Obtiene la conexión usando `await`
+        const connection: PoolConnection = await pool.getConnection();
+        console.log('Conexión exitosa a la base de datos');
+        
+        // Aquí puedes realizar las operaciones con la base de datos
+
+        // Finalmente, libera la conexión
+        connection.release();
+    } catch (err) {
+        console.error('Error de conexión a la base de datos:', err);
+    }
+}
+
+// Llama a la función para probar la conexión
+testConnection();
