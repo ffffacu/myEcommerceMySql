2ed92fff44e639aad965e507902343b52398b1de
ffffacu <ffffacu@hotmail.com>
2025-05-05 20:01:31 -0300

    repart_db25

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,8 +1,8 @@
-const PORT = process.env.PORT || 3000
-const DB_HOST = process.env.DB_HOST
-const DB_USER = process.env.DB_USER || 'root'
-const DB_PASSWORD = process.env.DB_PASSWORD || '1560067488'
-const DB_DATABASE = process.env.DB_DATABASE || 'myEcommerce'
-const DB_PORT = process.env.DB_PORT || 3306
+const PORT= process.env.PORT || 3000
+const DB_HOST= process.env.DB_HOST
+const DB_USER= process.env.DB_USER || 'root'
+const DB_PASSWORD= process.env.DB_PASSWORD || '1560067488'
+const DB_DATABASE= process.env.DB_DATABASE || 'myEcommerce'
+const DB_PORT= process.env.DB_PORT || 3306
 
 export default {PORT, DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT}
