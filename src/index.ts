59c929b2be6f2d2899f7687ef6a43cdfd397eeb7
ffffacu <ffffacu@hotmail.com>
2025-05-19 22:35:03 -0300

    consultasCodigoFacilito v1.0.11

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,8 @@ import { testConnection } from './lib/db';
 //import { authenticatorToken } from './middlewares/authenticatorSession.middlewares';
 import cookieParser from 'cookie-parser';
 import env from "./config/config";
-import swaggerUiExpress from 'swagger-ui-express';
-import { specs } from './config/swagger.config';
+//import swaggerUiExpress from 'swagger-ui-express';
+//import { specs } from './config/swagger.config';
 
 import cors from 'cors';
 
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 
-app.use(`/api-docs`, swaggerUiExpress.serve, swaggerUiExpress.setup(specs));
+//app.use(`/api-docs`, swaggerUiExpress.serve, swaggerUiExpress.setup(specs));
 app.use(`/api`, router);
 
 
